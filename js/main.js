35c39da5a80fda962f78701ba82e51e3eb801280
adxamov1 <adxamovminxojidin@gmail.com>
2024-02-08 21:29:50 +0300

    js1dars

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var str = prompt('Hush kelibsiz')
+var ism = prompt('Ismizi kiriting')
+var age = prompt('Yoshingizni kiriting')
+var number = prompt('Raqmizni kiriting')
+console.log(ism);
+console.log(age);
+console.log(number);
